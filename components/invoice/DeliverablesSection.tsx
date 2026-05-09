d85a9d9d1ed98f0bff1026444ccf624e80fe69e7
Brijesh Kumar <kumar.brijesh900@gmail.com>
2026-05-09 15:43:41 +0530

    fix: resolve currency symbol overlap in rate field with forced padding

--- components/invoice/DeliverablesSection.tsx
+++ components/invoice/DeliverablesSection.tsx
@@ -444,14 +444,14 @@ function LineItemCard({
               )}
             </label>
             <div className="relative">
-              <span className="absolute left-3 top-1/2 -translate-y-1/2 text-sm text-gray-400 font-medium pointer-events-none">
+              <span className="absolute left-3.5 top-1/2 -translate-y-1/2 text-sm text-gray-400 font-medium pointer-events-none z-10">
                 {getCurrencySymbol(currency)}
               </span>
               <AppTextField
                 type="number"
                 value={item.rate}
                 className={cn(
-                  "h-10 text-sm pl-[34px] w-full",
+                  "h-10 text-sm !pl-10 w-full",
                   getInputStateClass(`deliverables.${itemIndex}.rate`, item.rate)
                 )}
                 errorText={(showAllErrors || touchedFields[`${item.id}:rate`]) ? errors?.rate : undefined}
